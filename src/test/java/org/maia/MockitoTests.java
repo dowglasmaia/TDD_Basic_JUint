d8ac3a85b2b226ef0ddf3508a51b8719ba036c4b
Dowglas Maia <dowglasmaia@gmail.com>
2020-04-16 23:13:59 -0300

    Mockito com anotações

--- src/test/java/org/maia/MockitoTests.java
+++ src/test/java/org/maia/MockitoTests.java
@@ -1,25 +1,29 @@
 package org.maia;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MockitoTests {
-	
-	@SuppressWarnings("unchecked")
+
+	@Mock
+	List<String> lista; // criando instancia da lista com mock
+
 	@Test
 	public void primeiroTesteMockito() {
-		List<String> lista = Mockito.mock(ArrayList.class); // criando instancia da lista com mock
-		
-		Mockito.when( lista.size()).thenReturn(20); // definindo o retorno da lista com mock
-		
+
+		Mockito.when(lista.size()).thenReturn(20); // definindo o retorno da lista com mock
+
 		int size = lista.size(); // atribuindo o valor da minha lista a uma variavel int
-		
-		Assertions.assertThat(size).isEqualTo(20); // virificando  e validando o tamanho da minha lista.
-		
+
+		Assertions.assertThat(size).isEqualTo(20); // virificando e validando o tamanho da minha lista.
+
 	}
 
 }
